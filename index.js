4a2a408f1e4fbb4d957b3e91a920f8fd779ae1eb
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-12-25 21:45:40 -0400

    Output for proxy port.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var argo = require('argo');
 var log = require('./log');
 
+var proxyPort = 44332;
+
+console.log("Exposed port: " + proxyPort);
+
 argo()
   .use(log)
   .target('http://flock-test-patcon.ngrok.com')
-  .listen(44332);
+  .listen(proxyPort);
